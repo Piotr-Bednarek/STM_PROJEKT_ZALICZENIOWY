54cf20b2840659916fbf912cc0e6b0ef137d95d8
Piotr Bednarek <72524343+Piotr-Bednarek@users.noreply.github.com>
2026-05-05 23:42:42 +0200

    update 2

--- 3-docs/convert_webp.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from PIL import Image
-
-docs_dir = Path(__file__).parent
-
-for webp in docs_dir.rglob("*.webp"):
-    png = webp.with_suffix(".png")
-    Image.open(webp).save(png, "PNG")
-    print(f"{webp.relative_to(docs_dir)} -> {png.name}")
